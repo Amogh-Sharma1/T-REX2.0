7ec17453e2b4d70d40df501db00309bb62634326
Amogh-Sharma1 <86812220+Amogh-Sharma1@users.noreply.github.com>
2021-07-02 09:38:38 +0800

    Add files via upload

---
+++ T-REX/sketch.js
@@ -0,0 +1,16 @@
+
+function setup() {
+  createCanvas(400,400);
+  var ball = createSprite(200,200,300,300)
+  ball.shapeColor = 'white'
+}
+
+function draw() 
+{
+  background(30);
+drawSprites()
+}
+
+
+
+
